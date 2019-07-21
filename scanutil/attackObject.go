8184e9fe66988d40be8ab445920c3177f8a4501f
Charlie <charlie.belmer@protonmail.com>
2019-07-20 20:22:39 -0400

    Adding ability to add injection payloads to request body for urlencoded form data and JSON body data

--- scanutil/attackObject.go
+++ scanutil/attackObject.go
@@ -23,46 +23,116 @@ import (
     "io/ioutil"
     "log"
     "errors"
+    "time"
+    "bufio"
+    "os"
+    "bytes"
+    "strings"
+    "regexp"
 )
 
 /**
  *  An object to manage attack payloads and structures. 
  */
-type attackObject struct {
-    URL *url.URL
-    body string
-}
-
-type HTTPResponseObject struct {
-    Url string
+type AttackObject struct {
+	Request *http.Request
+    Client *http.Client
+    Options ScanOptions
     Body string
-    Header map[string][]string
-    StatusCode int
+    originalBody string  // Keep the original body, so we can reset it after injecting attack strings.
+    BodyValues []string  // List of all values that can be updated. May include maps or arrays (if body is JSON) - but as Strings
 }
 
-func NewAttackObject(u string) attackObject {
-   obj := attackObject{}
-   obj.SetURL(u)
-   obj.body = ""
-   return obj
+func NewAttackObject(options ScanOptions) (AttackObject, error) {
+	attackObj := AttackObject{}
+
+	if options.Request != "" {
+        attackObj = parseRequest(options.Request)
+    } else if options.Target != "" {
+        var err error
+	    attackObj.Request, err = http.NewRequest("", options.Target, nil)
+	    if err != nil {
+	    	log.Fatal(err)
+	    }
+    } else {
+        return attackObj, errors.New("You must specify either a target or a request file to scan.")
+    }
+
+    if options.RequestData != "" {
+    	attackObj.SetBody(options.RequestData)
+    }
+
+    attackObj.Options = options
+    attackObj.addClient()
+    attackObj.Request.Header.Set("User-Agent", options.UserAgent())
+    return attackObj, nil
+}
+
+func parseRequest(file string) AttackObject {
+	obj := AttackObject{}
+	fh, err := os.Open(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	data := bufio.NewReader(fh)
+	obj.Request, err = http.ReadRequest(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Update the request to make sure it is properly formed
+	obj.Request.RequestURI = ""
+	obj.Request.URL, err = url.Parse("http://" + obj.Request.Host + obj.Request.URL.String())
+	if err != nil {
+		log.Fatal( err )
+	}
+	obj.addClient()
+
+
+	buf := new(bytes.Buffer)
+    buf.ReadFrom(obj.Request.Body)
+    obj.Body = 	buf.String()
+
+    return obj
+}
+
+/**
+ *	Add a default client object to the attack object
+ */
+func (a *AttackObject) addClient() {
+	proxy := a.Options.Proxy()
+	transport := &http.Transport{}
+	
+	if proxy != "" {
+		proxyURL, err := url.Parse(proxy)
+		if err != nil {
+			log.Fatal("Proxy not set correctly: %s", err)
+		}
+		fmt.Printf("Using proxy %s\n", proxyURL)
+		transport.Proxy = http.ProxyURL(proxyURL)
+	}
+	a.Client = &http.Client{
+        Timeout: 30 * time.Second,
+        Transport: transport,
+    }
 }
 
 /**
  * Parse a URL into an attack object
  */
-func (a *attackObject) SetURL(u string) {
+func (a *AttackObject) SetURL(u string) {
 	parsedURL, err := url.Parse(u)
     if err != nil {
         log.Fatal(err)
     }
-    a.URL = parsedURL
+    a.Request.URL = parsedURL
 }
 
 /**
  *	Return a list of query parameters in a map param: value
  */
-func (a *attackObject) QueryParams() map[string]string {
-	q := a.URL.Query()
+func (a *AttackObject) QueryParams() map[string]string {
+	q := a.Request.URL.Query()
 	m := map[string]string{}
 	for k, v := range q {
 		m[k] = v[0]
@@ -71,30 +141,199 @@ func (a *attackObject) QueryParams() map[string]string {
 }
 
 /**
- * Change a query param in location paramLoc. Use this to 
+ * Change a query param in the request query string. Use this to 
  * attempt nosql injection via GET paramaters.
  */
-func (a *attackObject) SetQueryParam(key string, payload string) {
-    q := a.URL.Query()
+func (a *AttackObject) SetQueryParam(key string, payload string) {
+    q := a.Request.URL.Query()
     q[key][0] = payload
-    a.URL.RawQuery = q.Encode()
+    a.Request.URL.RawQuery = q.Encode()
 }
+
+/**
+ * Replace a pattern with an injection payload within body data, assuming the
+ * data is form URL encoded.
+ */
+func (a *AttackObject) setBodyQueryParam(pattern string, payload string, replaceKey bool) error {
+	u, err := url.ParseRequestURI("/?" + a.Body)
+	if err != nil {
+		return err
+	}
+	q := u.Query()
+	for key, vSlice := range q {
+		if replaceKey && key == pattern {
+			q[payload] = vSlice
+			delete(q, key)
+		} else {
+			for i,v := range vSlice {
+				if url.QueryEscape(v) == pattern {
+					vSlice[i] = payload
+				}
+			}
+			q[key] = vSlice
+		}
+	}
+	a.Body = q.Encode()
+	return nil
+}
+
+/**
+ * Replace part of the JSON object with a payload
+ * The payload should be proper JSON as it will completely replace an existing element - 
+ * if the payload is a string, it should include double quotes, like `"payload"`, if an
+ * object, include the full object notation such as `{"$ne": 1}`
+ */
+func (a *AttackObject) setBodyJSONParam(pattern string, payload string, replaceKey bool) error {
+	switch jsonType(pattern) {
+	case "string":
+		// string should be surrounded by double quotes
+		pattern = `"` + pattern + `"`
+		re := regexp.MustCompile(pattern)
+		a.Body = re.ReplaceAllString(a.Body, payload)
+	case "number", "boolean", "null":
+		// objects that are not enclosed with quotes should always be values (not keys)
+		// and thus prefixed with a colon or object opener and zero or more spaces
+		// they also should be followed by a comma, or closure of an array or object.
+		pattern = `(?P<Prefix>[\[,:]\s*?)(?P<Payload>` + pattern + `)(?P<Suffix>\s*?[,\]\}])`
+		re := regexp.MustCompile(pattern)
+		submatches := re.FindAllStringSubmatch(a.Body, -1)
+		names := re.SubexpNames()
+		m := map[string]string{}
+		m2 := []map[string]string{}
+		// If we have multiple matches, they may have differing prefixes and suffixes
+		// so we'll go through and create a new regex and payload to exact match each.
+		for _, submatch := range submatches {
+			m = make(map[string]string)
+			for i, n := range submatch {
+	            m[names[i]] = n
+	        }
+	        m2 = append(m2,m)
+	    }
+		var newRegex string
+		var newPayload string
+		for _, finding := range m2 {
+			newRegex = finding["Prefix"] + finding["Payload"] + finding["Suffix"]
+			newPayload = finding["Prefix"] + payload + finding["Suffix"]
+			re = regexp.MustCompile(newRegex)
+			a.Body = re.ReplaceAllString(a.Body, newPayload)
+		}
+		
+	default:
+		// array or object should be ok as-is, just use string replace
+		a.Body = strings.ReplaceAll(a.Body, pattern, payload)
+	}
+	return nil
+}
+
+/**
+ * Find a pattern in the body, and replace it with the given payload.
+ * If the body is JSON, this will take into account proper ways of finding
+ * the data correctly. For instance, a string should always be surrounded by quotes,
+ * but an int should be surrounded by some combination of white space, commas, or object/array
+ * closures.
+ *
+ * This function assumes the pattern being replaced is a complete object - for instance 
+ * if the body contains '"key": "value"', we would only replace "key" or "value", but never 
+ * "alue"
+ *
+ * Only replaces keys if inKey is set to true, so that data like username=username can be 
+ * injected one at a time (key injections are rare)
+ */
+func (a *AttackObject) ReplaceBodyObject(pattern string, payload string, replaceKey bool) {
+	if a.bodyIsJSON() {
+		a.setBodyJSONParam(pattern, payload, replaceKey)
+	} else {
+		a.setBodyQueryParam(pattern, payload, replaceKey)
+	}
+}
+
+func (a *AttackObject) bodyIsJSON() bool {
+	contentType := a.Request.Header.Get("Content-Type")
+	return contentType == "application/json"
+}
+
 
 /**
  * Update the request body with a payload
  */
-func (a *attackObject) SetBody(body string) {
-    
+func (a *AttackObject) SetBody(body string) {
+	a.Body = body
+	a.originalBody = body
+
+	if a.Body == "" {
+		a.Request.Method = "GET"
+		return
+	} else {
+		a.Request.Method = "POST"
+	}
+
+	if isJSON(a.Body) {
+    	a.Request.Header.Set("Content-Type", "application/json")
+    	a.BodyValues = FlattenJSON(body)
+    } else {
+    	a.Request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+    	a.urlEncodeBody()
+    	a.extractUpdateableValuesFromBody(body)
+    }
+}
+
+/** 
+ * URL encode a body to it matches application/x-www-form-urlencoded
+ * content type.
+ */
+func (a *AttackObject) urlEncodeBody() {
+	u, err := url.ParseRequestURI("/?" + a.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	q := u.Query()
+	a.Body = q.Encode()
 }
 
 /**
- * Send a GET request to the object target
+ * Replace attack body with original (default) data
  */
-func (a *attackObject) GET() (HTTPResponseObject, error) {
-	url := a.URL.String()
-	fmt.Printf("Running GET request for %s\n", url)
+func (a *AttackObject) RestoreBody() {
+	a.Body = a.originalBody
+}
+
+/**
+ * Pull out any values we might want to replace with injection strings,
+ * and save them into the attack object.
+ */ 
+func (a *AttackObject) extractUpdateableValuesFromBody(body string){
+	u, err := url.ParseRequestURI("/?" + a.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	q := u.Query()
+	for k, v := range q {
+		a.BodyValues = append(a.BodyValues, k)
+		for _, val := range v {
+			a.BodyValues = append(a.BodyValues, val)
+		}
+	}
+}
+
+/**
+ * Update the request body with the body on the object.
+ * Encode if a query string is used.
+ */
+func (a *AttackObject) setRequestBody() {
+    a.Request.Body = ioutil.NopCloser(strings.NewReader(a.Body))
+}
+
+/**
+ * Send the request to the object target
+ */
+func (a *AttackObject) Send() (HTTPResponseObject, error) {
+	a.setRequestBody()
+	url := a.Request.URL.String()
+	fmt.Printf("Running request for %s\n", url)
 	obj := HTTPResponseObject{url, "", nil, 0}
-    resp, err := http.Get(url)
+
+    resp, err := a.Client.Do(a.Request)
+
     if err != nil {
         log.Fatal(err)
         return obj, errors.New("Unable to retrieve url")
@@ -114,11 +353,4 @@ func (a *attackObject) GET() (HTTPResponseObject, error) {
     obj.Body = string(body)
 
     return obj, nil
-}
-
-/**
- * Send a POST request to the object target
- */
-func (a *attackObject) POST() {
-
 }
---
+++ scanutil/jsonUtils.go
@@ -0,0 +1,90 @@
+/*
+Copyright © 2019 Charlie Belmer <Charlie.Belmer@protonmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package scanutil
+
+/**
+ * Because extracting and inserting attack strings into JSON bodies 
+ * is a little more complicated.
+ */
+
+import (
+	"github.com/buger/jsonparser"
+	"encoding/json"
+)
+
+func isJSON(s string) bool {
+	var js map[string]interface{}
+	err := json.Unmarshal([]byte(s), &js) 
+	return err == nil
+}
+
+func jsonArrayHandler(arrayData []byte, flattenedSlice []string) []string {
+	jsonparser.ArrayEach(arrayData, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+		flattenedSlice = append(flattenedSlice, string(value))
+		switch dataType.String() {
+		case "object":
+			flattenedSlice = jsonObjectHandler([]byte(string(value)), flattenedSlice)
+		case "array":
+			flattenedSlice = jsonArrayHandler(value, flattenedSlice)
+		}
+	})
+	return flattenedSlice
+}
+
+func jsonObjectHandler(jsonData []byte, flattenedSlice []string) []string {
+	jsonparser.ObjectEach(jsonData, func(key []byte, value []byte, dataType jsonparser.ValueType, offset int) error {
+		flattenedSlice = append(flattenedSlice, string(key))
+		flattenedSlice = append(flattenedSlice, string(value))
+		
+		switch dataType.String() {
+		case "object":
+			flattenedSlice = jsonObjectHandler(value, flattenedSlice)
+		case "array":
+			flattenedSlice = jsonArrayHandler(value, flattenedSlice)
+		}
+		return nil
+	})
+	return flattenedSlice
+} 
+
+/*
+ * Takes a JSON object (as a string), and returns all keys and values in a string slice
+ */
+func FlattenJSON(jsonData string) []string {
+	b := []byte(jsonData)
+	var s []string
+	return jsonObjectHandler(b, s)
+}
+
+/*
+ * Returns the type of object contained in the string. Could be any of the following
+ * 	string
+ *	number
+ *	object
+ *	array
+ *	boolean
+ *	null
+ */
+func jsonType(jsonData string) string {
+	b := []byte(jsonData)
+	_, vtype, _, err := jsonparser.Get(b)
+	if err != nil || vtype.String() == "unknown" {
+		return "string"
+	} else {
+		return vtype.String()
+	}
+}
