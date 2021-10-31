6fce3ebc8c8127940221d9287b00493be43d7564
Charlie Belmer <charlie.belmer@protonmail.com>
2021-10-31 06:37:50 -0400

    version bump

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-env GOOS=linux GOARCH=amd64 go build -o distribution/nosqli_linux_x64_v0.5.0 .
-env GOOS=linux GOARCH=386 go build -o distribution/nosqli_linux_x86_v0.5.0 .
-env GOOS=windows GOARCH=amd64 go build -o distribution/nosqli_windows_v0.5.0.exe .
-env GOOS=darwin GOARCH=amd64 go build  -o distribution/nosqli_macos_v0.5.0 .
+env GOOS=linux GOARCH=amd64 go build -o distribution/nosqli_linux_x64_v0.5.4 .
+env GOOS=linux GOARCH=386 go build -o distribution/nosqli_linux_x86_v0.5.4 .
+env GOOS=windows GOARCH=amd64 go build -o distribution/nosqli_windows_v0.5.4.exe .
+env GOOS=darwin GOARCH=amd64 go build  -o distribution/nosqli_macos_v0.5.4 .
